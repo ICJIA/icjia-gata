7c4674474a12b45542a4f644ed5640a606e29a19
Chris Schweda <cschweda@gmail.com>
2017-04-06 11:54:36 -0500

    Breadcrumb

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,14 +1,15 @@
 <template lang="html">
   <div>
+
     <nav class="breadcrumb" style="margin-top: -30px; margin-bottom: 80px">
       <span style="float: right;">
-          <router-link class="breadcrumb-item" to="/">Home</router-link>
+          <router-link class="breadcrumb-item" to="/">Home&nbsp;/&nbsp;</router-link>
 
-          <!-- <span v-for="section in url">
+          <span v-for="section in url">
             <router-link class="breadcrumb-item" :to="section">{{ section }}</router-link>
-          </span> -->
-          <router-link class="breadcrumb-item" to="/">Library</router-link>
-          <router-link class="breadcrumb-item" to="/">Test</router-link>
+          </span>
+          <!-- <router-link class="breadcrumb-item" to="/">Library</router-link>
+          <router-link class="breadcrumb-item" to="/">Test</router-link> -->
 
 </span>
 </nav>
@@ -21,8 +22,10 @@ export default {
   mounted () {
     let _obj = {}
     console.log(this.$route.fullPath)
-  
-
+    this.url = this.$route.fullPath.replace(/\/\s*$/,'').split('/');
+    //since we do not need example.com
+    this.url.shift();
+    console.log(this.url)
   },
   data () {
     return {
