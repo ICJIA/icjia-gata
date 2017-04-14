26df81d33ef5e60aef6ab973999dad3a681306b8
cschweda <cschweda@gmail.com>
2017-04-14 11:21:48 -0500

    Add 'allowfullscreen' to video embeds

--- src/views/Webinars.vue
+++ src/views/Webinars.vue
@@ -230,6 +230,17 @@
       </div>
 
 
+      <!-- <div class="video-embed">
+<div class="youtube"
+     id="nFVMlof28nU"
+     style="width: 340px; height: 220px;">
+
+
+</div>
+<div class="caption text-center">Video webinar title 1</div>
+</div> -->
+
+
 
 
 
@@ -298,11 +309,11 @@ $(function() {
 
         $(document).delegate('#'+this.id, 'click', function() {
             // Create an iFrame with autoplay set to true
-            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?fullscreen=1&autoplay=1&autohide=1";
+            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?allowfullscreen&autoplay=1&autohide=1";
             if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
 
             // The height and width of the iFrame should be the same as parent
-            var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': $(this).width(), 'height': $(this).height() })
+            var iframe = $('<iframe/>', {'frameborder': '0', 'allowfullscreen':'true','src': iframe_url, 'width': $(this).width(), 'height': $(this).height() })
 
             // Replace the YouTube thumbnail with YouTube HTML5 Player
             $(this).replaceWith(iframe);
