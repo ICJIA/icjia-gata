34c9efdc79a54c589e21f5ded250e078fd90ab21
Chris Schweda <cschweda@gmail.com>
2017-08-04 13:36:31 -0500

    Tweak format

--- src/generatePrerenderRoutes.js
+++ src/generatePrerenderRoutes.js
@@ -1,7 +1,7 @@
 const dirTree = require('directory-tree');
 const _ = require('lodash')
 var path = require('path')
-String.prototype.camelCaseToDashed = function(){
+String.prototype.camelCaseToDashed = function () {
   return this.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 }
 // Usage
@@ -10,27 +10,28 @@ const tree = dirTree('./src/views', ['.vue'], (item, PATH) => {
   x = String(item.path).camelCaseToDashed();
 
   //TODO: Convert to regex
-  x = _.replace(x,"src\\","")
-  x = _.replace(x,"views\\","")
-  x = _.replace(x,"src/","")
-  x = _.replace(x,"views/","")
-  x = _.replace(x,".vue","")
-  x = _.replace(x,"\\","/")
-  x = _.replace(x,"\\","/")
+  x = _.replace(x, "src\\", "")
+  x = _.replace(x, "views\\", "")
+  x = _.replace(x, "src/", "")
+  x = _.replace(x, "views/", "")
+  x = _.replace(x, ".vue", "")
+  x = _.replace(x, "\\", "/")
+  x = _.replace(x, "\\", "/")
 
   // TODO: Check for underscore prefix on filename to ignore page for prerendering
   if (x != 'home') {
-    arr.push(JSON.stringify('/'+x));
+    arr.push(JSON.stringify('/' + x));
   }
 });
 //console.log(arr)
 myFile = 'module.exports = [' + arr + ']';
 
 var fs = require('fs');
-fs.writeFile("./src/routesToPrerender.js", myFile, function(err) {
-    if(err) {
-        return console.log(err);
-    }
+fs.writeFile("./src/routesToPrerender.js", myFile, function (err) {
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log("routesToPrerender.js was saved.");
+  console.log("routesToPrerender.js was saved.");
 });
+
