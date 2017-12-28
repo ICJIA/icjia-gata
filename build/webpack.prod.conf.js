7116d09febadcc8118748b15b4202bd4d672cba1
Chris Schweda <cschweda@gmail.com>
2017-12-28 11:28:42 -0600

    Add Google file

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,26 +1,24 @@
-var path = require('path')
-var utils = require('./utils')
-var webpack = require('webpack')
-var config = require('../config')
-var merge = require('webpack-merge')
-var baseWebpackConfig = require('./webpack.base.conf')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var moment = require('moment-timezone');
-var PrerenderSpaPlugin = require('prerender-spa-plugin');
-var routesToPrerender = require('../src/routesToPrerender');
-var template = require('../template')
+var path = require("path");
+var utils = require("./utils");
+var webpack = require("webpack");
+var config = require("../config");
+var merge = require("webpack-merge");
+var baseWebpackConfig = require("./webpack.base.conf");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var OptimizeCSSPlugin = require("optimize-css-assets-webpack-plugin");
+var moment = require("moment-timezone");
+var PrerenderSpaPlugin = require("prerender-spa-plugin");
+var routesToPrerender = require("../src/routesToPrerender");
+var template = require("../template");
 
 // console.log(routesToPrerender)
 
-
-
-
-var env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : config.build.env
+var env =
+  process.env.NODE_ENV === "testing"
+    ? require("../config/test.env")
+    : config.build.env;
 
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -29,16 +27,16 @@ var webpackConfig = merge(baseWebpackConfig, {
       extract: true
     })
   },
-  devtool: config.build.productionSourceMap ? '#source-map' : false,
+  devtool: config.build.productionSourceMap ? "#source-map" : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath("js/[name].[chunkhash].js"),
+    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js")
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      "process.env": env
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -48,7 +46,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath("css/[name].[contenthash].css")
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
@@ -59,79 +57,84 @@ var webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin(template.build),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
+      name: "vendor",
+      minChunks: function(module, count) {
         // any required modules inside node_modules are extracted to vendor
         return (
           module.resource &&
           /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
+          module.resource.indexOf(path.join(__dirname, "../node_modules")) === 0
+        );
       }
     }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
+      name: "manifest",
+      chunks: ["vendor"]
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '../static'),
+        from: path.resolve(__dirname, "../static"),
         to: config.build.assetsSubDirectory,
-        ignore: ['.*']
+        ignore: [".*"]
       }
     ]),
 
     //
     new CopyWebpackPlugin([
-            {
-              from: path.resolve(__dirname, '../_redirects'),
-              to: './' },
-        ]),
+      {
+        from: path.resolve(__dirname, "../_redirects"),
+        to: "./"
+      }
+    ]),
     new CopyWebpackPlugin([
-                {
-                  from: path.resolve(__dirname, '../.htaccess'),
-                  to: './' },
-            ]),
+      {
+        from: path.resolve(__dirname, "../google0bc0c1c6e02aa032.html"),
+        to: "./"
+      }
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "../.htaccess"),
+        to: "./"
+      }
+    ])
   ]
-})
+});
 
 if (config.build.productionGzip) {
-  var CompressionWebpackPlugin = require('compression-webpack-plugin')
+  var CompressionWebpackPlugin = require("compression-webpack-plugin");
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
       test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
+        "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
       ),
       threshold: 10240,
       minRatio: 0.8
     })
-  )
+  );
 }
 
 if (config.build.bundleAnalyzerReport) {
-  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+  var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
 if (config.build.prerender) {
-
-  webpackConfig.plugins.push(new PrerenderSpaPlugin(
+  webpackConfig.plugins.push(
+    new PrerenderSpaPlugin(
       // Absolute path to compiled SPA
-      path.join(__dirname, '../dist'),
+      path.join(__dirname, "../dist"),
       routesToPrerender,
-      { captureAfterTime: 15000}
-
-    ))
+      { captureAfterTime: 15000 }
+    )
+  );
 }
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
