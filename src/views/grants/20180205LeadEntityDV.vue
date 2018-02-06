9d684ef50910733e8765178f55cb8b6960779756
Chris Schweda <cschweda@gmail.com>
2018-02-06 09:55:48 -0600

    Add NOFOs

--- src/views/grants/20180205LeadEntityDV.vue
+++ src/views/grants/20180205LeadEntityDV.vue
@@ -1,236 +1,265 @@
 <template lang="html">
-  <section class="page">
-    <page-header header="Notice of Funding Opportunity" :expired="expired" />
+    <section class="page">
+        <page-header header="Notice of Funding Opportunity" :expired="expired" />
 
-    <div class="container-fluid side-spacer">
-      <div class="row">
-
-
-        <div class="col-md-3 hidden-sm-down">
-
-          <ul class="sticky nav flex-column sidenav-toc">
-            <li class="nav-item">
-              <a class="nav-link active" href="#app">Introduction</a>
-            </li>
+        <div class="container-fluid side-spacer">
+            <div class="row">
 
 
+                <div class="col-md-3 hidden-sm-down">
 
-  <li class="nav-item">
-    <a class="nav-link" href="#eligibility">Eligibility</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#notice-of-funding-opportunity">Notice of Funding Opportunity</a>
-  </li>
-
- <li class="nav-item">
-    <a class="nav-link" href="#deadline">Deadline</a>
-  </li>
+                    <ul class="sticky nav flex-column sidenav-toc">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#app">Introduction</a>
+                        </li>
 
 
-  <li class="nav-item">
-    <a class="nav-link" href="#timeline">Timeline</a>
-  </li>
-<!-- 
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#eligibility">Eligibility</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#notice-of-funding-opportunity">Notice of Funding Opportunity</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#deadline">Deadline</a>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#timeline">Timeline</a>
+                        </li>
+                        <!-- 
   <li class="nav-item">
     <a class="nav-link" href="#download-application-documents">Download Application Documents</a>
   </li> -->
 
-   <!-- <li class="nav-item">
+                        <!-- <li class="nav-item">
     <a class="nav-link" href="#updates-and-responses-to-applicant-questions">Updates and Responses to Applicant Questions</a>
   </li>  -->
 
-   <li class="nav-item">
-    <a class="nav-link" href="#reporting">Reporting</a>
-  </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#reporting">Reporting</a>
+                        </li>
 
 
-</ul>
-        </div>
-
-
-
+                    </ul>
+                </div>
 
 
 
 
-        <div class="col-md-9">
-          <div id="grant-posting-new">
+
+
+
+                <div class="col-md-9">
+                    <div id="grant-posting-new">
 
 
 
 
-<h1 class="spacerBottom50">{{title}}</h1>
+                        <h1 class="spacerBottom50">{{title}}</h1>
 
-<!-- START CONTENT: --------------------------------------------------
+                        <!-- START CONTENT: --------------------------------------------------
 
     Step 1: Enter HTML here
     Step 2: Update routing in routes.js
 
 --------------------------------------------------------------------- -->
 
-<!-- <h1 id="notice-of-funding-opportunity-federal-fiscal-year-2015-victimof-crime-act-lead-entity-services-for-victims-of-domestic-violence-program">NOTICE OF FUNDING OPPORTUNITY: FEDERAL FISCAL YEAR 2015 VICTIMOF CRIME ACT LEAD ENTITY SERVICES FOR VICTIMS OF DOMESTIC VIOLENCE PROGRAM</h1> -->
-<h2 id="introduction">Introduction</h2>
-<p>ICJIA is the state agency charged with the administration of the Illinois’ Victims of Crime Act Victim (VOCA) Assistance Formula Grant Program. This program is supported by fines and penalties levied against criminals convicted of federal crimes and allocated
-    to states by formula by the Office for Victims of Crime of the U.S. Department of Justice. In federal fiscal year 2016, Illinois received a VOCA award of $87 million.</p>
-<p>VOCA grants must support the provision of direct services to victims of violent crime. States are required to allocate a minimum of 10 percent of funds received for services to each of the following: victims of sexual assault, domestic violence, child
-    abuse, and underserved victims of violent crime. VOCA funds may not be used to supplant or replace state and local funds that would otherwise be available for crime victim services and must be used to develop new projects or expand existing projects.</p>
-<p>For more information on the VOCA Program please visit: <a href="http://ojp.gov/ovc/about/victimsfund.html">http://ojp.gov/ovc/about/victimsfund.html</a>.</p>
-<p>Last year, ICJIA completed a statewide assessment of victim services in Illinois. The report was presented to the Ad Hoc Victim Services Committee for consultation and coordination with other state agencies and victim stakeholder groups. The final report’s
-    recommendations were approved by the ICJIA Board in January 2017. These recommendations outline a comprehensive plan to address victims’ needs in Illinois and are reflected in this funding opportunity Please see the following link to access the report
-    and recommendations: <a href="http://www.icjia.state.il.us/articles/ad-hoc-victim-services-committee-research-report">http://www.icjia.state.il.us/articles/ad-hoc-victim-services-committee-research-report</a>.</p>
-<p>The Ad Hoc Victim Services Committee made a priority of strengthening and expansion of core services for victims of crime. To fulfill this priority, ICJIA made available $17.3 million in VOCA grant funding for core services and related support costs for
-    victims of domestic violence. These funds are available to the Illinois Coalition Against Domestic Violence.</p>
-<h2 id="eligibility">Eligibility</h2>
-<p>This solicitation is open to the Illinois Coalition Against Domestic Violence, 806 S. College Street, Springfield, Illinois, 62704.</p>
-<h2 id="notice-of-funding-opportunity">Notice of Funding Opportunity</h2>
-<p>The Notice of Funding Opportunity and application materials were provided to the Illinois Coalition Against Domestic Violence.</p>
-<h2 id="deadline">Deadline</h2>
-<p>Application materials must be submitted to Ronnie Reichgelt at <a href="mailto:Ronnie.Reichgelt@Illinois.gov">Ronnie.Reichgelt@Illinois.gov</a> by 11:59 p.m., Friday, March 09, 2018.</p>
-<h2 id="available-funds">Available Funds</h2>
-<p>A total of $17.3 million in FFY16 VOCA funding is available through this solicitation.</p>
-<p>Agreements that result from this funding opportunity are contingent upon and subject to the availability of funds.</p>
-<h2 id="period-of-performance">Period of Performance</h2>
-<p>The award resulting from this opportunity will have a period of performance of July 1, 2018, through September 30, 2019.</p>
-<h2 id="timeline">Timeline:</h2>
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th style="text-align:center">Task</th>
-            <th style="text-align:center">Date</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td style="text-align:center">NOFO posted</td>
-            <td style="text-align:center">Wednesday, February 07, 2018</td>
-        </tr>
-        <tr>
-            <td style="text-align:center"><strong>Application deadline</strong></td>
-            <td style="text-align:center"><strong>11:59 p.m., Friday, March 09, 2018</strong></td>
-        </tr>
-        <tr>
-            <td style="text-align:center">Authority Budget Committee meeting</td>
-            <td style="text-align:center">May 25, 2018</td>
-        </tr>
-        <tr>
-            <td style="text-align:center"><strong>Program start date</strong></td>
-            <td style="text-align:center"><strong>July 1, 2018</strong></td>
-        </tr>
-    </tbody>
-</table>
-<h2 id="reporting">Reporting</h2>
-<p>The funded agency will be required to submit to ICJIA monthly fiscal and quarterly data reports.</p>
+                        <!-- <h1 id="notice-of-funding-opportunity-federal-fiscal-year-2015-victimof-crime-act-lead-entity-services-for-victims-of-domestic-violence-program">NOTICE OF FUNDING OPPORTUNITY: FEDERAL FISCAL YEAR 2015 VICTIMOF CRIME ACT LEAD ENTITY SERVICES FOR VICTIMS OF DOMESTIC VIOLENCE PROGRAM</h1> -->
+                        <h2 id="introduction">Introduction</h2>
+                        <p>ICJIA is the state agency charged with the administration of the Illinois’ Victims of Crime Act Victim
+                            (VOCA) Assistance Formula Grant Program. This program is supported by fines and penalties levied
+                            against criminals convicted of federal crimes and allocated to states by formula by the Office
+                            for Victims of Crime of the U.S. Department of Justice. In federal fiscal year 2016, Illinois
+                            received a VOCA award of $87 million.</p>
+                        <p>VOCA grants must support the provision of direct services to victims of violent crime. States are
+                            required to allocate a minimum of 10 percent of funds received for services to each of the following:
+                            victims of sexual assault, domestic violence, child abuse, and underserved victims of violent
+                            crime. VOCA funds may not be used to supplant or replace state and local funds that would otherwise
+                            be available for crime victim services and must be used to develop new projects or expand existing
+                            projects.
+                        </p>
+                        <p>For more information on the VOCA Program please visit:
+                            <a href="http://ojp.gov/ovc/about/victimsfund.html">http://ojp.gov/ovc/about/victimsfund.html</a>.</p>
+                        <p>Last year, ICJIA completed a statewide assessment of victim services in Illinois. The report was
+                            presented to the Ad Hoc Victim Services Committee for consultation and coordination with other
+                            state agencies and victim stakeholder groups. The final report’s recommendations were approved
+                            by the ICJIA Board in January 2017. These recommendations outline a comprehensive plan to address
+                            victims’ needs in Illinois and are reflected in this funding opportunity Please see the following
+                            link to access the report and recommendations:
+                            <a href="http://www.icjia.state.il.us/articles/ad-hoc-victim-services-committee-research-report">http://www.icjia.state.il.us/articles/ad-hoc-victim-services-committee-research-report</a>.</p>
+                        <p>The Ad Hoc Victim Services Committee made a priority of strengthening and expansion of core services
+                            for victims of crime. To fulfill this priority, ICJIA made available $17.3 million in VOCA grant
+                            funding for core services and related support costs for victims of domestic violence. These funds
+                            are available to the Illinois Coalition Against Domestic Violence.</p>
+                        <h2 id="eligibility">Eligibility</h2>
+                        <p>This solicitation is open to the Illinois Coalition Against Domestic Violence, 806 S. College Street,
+                            Springfield, Illinois, 62704.</p>
+                        <h2 id="notice-of-funding-opportunity">Notice of Funding Opportunity</h2>
+                        <p>The Notice of Funding Opportunity and application materials were provided to the Illinois Coalition
+                            Against Domestic Violence.</p>
+                        <a href="/static/grants/20180205LeadEntityDV/VOCA Lead Entity NOFO - DV - Final  - 5.0.docx">View the Notice of Funding Opportunity</a>
+                        <h2 id="deadline">Deadline</h2>
+                        <p>Application materials must be submitted to Ronnie Reichgelt at
+                            <a href="mailto:Ronnie.Reichgelt@Illinois.gov">Ronnie.Reichgelt@Illinois.gov</a> by 11:59 p.m., Friday, March 09, 2018.</p>
+                        <h2 id="available-funds">Available Funds</h2>
+                        <p>A total of $17.3 million in FFY16 VOCA funding is available through this solicitation.</p>
+                        <p>Agreements that result from this funding opportunity are contingent upon and subject to the availability
+                            of funds.</p>
+                        <h2 id="period-of-performance">Period of Performance</h2>
+                        <p>The award resulting from this opportunity will have a period of performance of July 1, 2018, through
+                            September 30, 2019.</p>
+                        <h2 id="timeline">Timeline:</h2>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th style="text-align:center">Task</th>
+                                    <th style="text-align:center">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style="text-align:center">NOFO posted</td>
+                                    <td style="text-align:center">Wednesday, February 07, 2018</td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:center">
+                                        <strong>Application deadline</strong>
+                                    </td>
+                                    <td style="text-align:center">
+                                        <strong>11:59 p.m., Friday, March 09, 2018</strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:center">Authority Budget Committee meeting</td>
+                                    <td style="text-align:center">May 25, 2018</td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:center">
+                                        <strong>Program start date</strong>
+                                    </td>
+                                    <td style="text-align:center">
+                                        <strong>July 1, 2018</strong>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h2 id="reporting">Reporting</h2>
+                        <p>The funded agency will be required to submit to ICJIA monthly fiscal and quarterly data reports.</p>
 
 
-<!-- //END CONTENT --------------------------------------------------- -->
+                        <!-- //END CONTENT --------------------------------------------------- -->
 
-          </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
         </div>
-      </div>
 
-    </div>
-
-<sticky-sidebar />
-  <smooth-scroll />
+        <sticky-sidebar />
+        <smooth-scroll />
 
 
 
 
-  </section>
+    </section>
 
-</section>
+    </section>
 </template>
 
 <script>
-import utilities from "@/utilities";
-import PageHeader from "@/components/PageHeader";
-import SmoothScroll from "@/components/SmoothScroll";
-import StickySidebar from "@/components/StickySidebar";
-//import StickySidebar from '@/components/StickySidebar'
+    import utilities from "@/utilities";
+    import PageHeader from "@/components/PageHeader";
+    import SmoothScroll from "@/components/SmoothScroll";
+    import StickySidebar from "@/components/StickySidebar";
+    //import StickySidebar from '@/components/StickySidebar'
 
-const description = `ICJIA is the state agency charged with the administration of the Illinois’ Victims of Crime Act (VOCA) Victim Assistance Formula Grant Program. This program is supported by fines and penalties levied against criminals convicted of federal crimes and allocated
+    const description = `ICJIA is the state agency charged with the administration of the Illinois’ Victims of Crime Act (VOCA) Victim Assistance Formula Grant Program. This program is supported by fines and penalties levied against criminals convicted of federal crimes and allocated
     to states by formula by the Office for Victims of Crime of the U.S. Department of Justice. In federal fiscal year 2016, Illinois received a VOCA award of $87 million.`;
-const title = utilities.titleCase(
-  "FEDERAL FISCAL YEAR 2016 VICTIMOF CRIME ACT LEAD ENTITY SERVICES FOR VICTIMS OF DOMESTIC VIOLENCE PROGRAM"
-);
-const created = "2018-02-05";
-const posted = "2018-02-05";
-const expired = "2018-03-09";
-const type = "grant";
-const status = "live";
+    const title = utilities.titleCase(
+        "FEDERAL FISCAL YEAR 2016 VICTIMOF CRIME ACT LEAD ENTITY SERVICES FOR VICTIMS OF DOMESTIC VIOLENCE PROGRAM"
+    );
+    const created = "2018-02-05";
+    const posted = "2018-02-05";
+    const expired = "2018-03-09";
+    const type = "grant";
+    const status = "live";
 
-export default {
-  name: utilities.camelCase(utilities.nameCheck(title)),
-  pageData: {
-    created,
-    description,
-    posted,
-    expired,
-    title,
-    type,
-    status
-  },
-  mounted() {},
-  metaInfo: {
-    // title will be injected into parent titleTemplate
-    title,
-    meta: [
-      {
-        vmid: "description",
-        name: "description",
-        content: utilities.stripTags(description)
-      }
-    ]
-  },
-  components: {
-    PageHeader,
-    SmoothScroll,
-    StickySidebar
-  },
-  data() {
-    return {
-      title,
-      expired
+    export default {
+        name: utilities.camelCase(utilities.nameCheck(title)),
+        pageData: {
+            created,
+            description,
+            posted,
+            expired,
+            title,
+            type,
+            status
+        },
+        mounted() { },
+        metaInfo: {
+            // title will be injected into parent titleTemplate
+            title,
+            meta: [
+                {
+                    vmid: "description",
+                    name: "description",
+                    content: utilities.stripTags(description)
+                }
+            ]
+        },
+        components: {
+            PageHeader,
+            SmoothScroll,
+            StickySidebar
+        },
+        data() {
+            return {
+                title,
+                expired
+            };
+        }
     };
-  }
-};
 </script>
 
 <style lang="css" scoped>
     h2 {
         margin-top: 60px !important;
     }
-    
+
     h3 {
         margin-top: 40px !important
     }
-    
+
     #grant-posting-new a {
         padding-bottom: 10px;
         text-decoration: underline;
     }
-    
+
     a.nav-link {
         text-decoration: none
     }
-    
+
     #grant-posting-new a {
         color: #2c2fb3 !important
     }
-    
+
     table {
         margin-bottom: 50px;
     }
-    
+
     a.fix {
         margin-top: 30px;
         margin-bottom: 50px;
         font-weight: 700;
         text-decoration: none !important
     }
-    
+
     a.btn-primary {
         text-decoration: none !important
     }
