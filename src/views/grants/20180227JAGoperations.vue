19bfc896a0f41986c70bd8631157e883c48ec197
Chris Schweda <cschweda@gmail.com>
2018-02-27 13:06:36 -0600

    Add new NOFO

---
+++ src/views/grants/20180227JAGoperations.vue
@@ -0,0 +1,400 @@
+<template lang="html">
+  <section class="page">
+    <page-header header="Notice of Funding Opportunity" :expired="expired" />
+
+    <div class="container-fluid side-spacer">
+      <div class="row">
+
+
+        <div class="col-md-3 hidden-sm-down">
+
+          <ul class="sticky nav flex-column sidenav-toc">
+            <li class="nav-item">
+              <a class="nav-link active" href="#app">Introduction</a>
+            </li>
+
+
+
+            <li class="nav-item">
+              <a class="nav-link" href="#eligibility">Eligibility</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#notice-of-funding-opportunity">Notice of Funding Opportunity</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#deadline">Deadline</a>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link" href="#timeline">Timeline</a>
+            </li>
+            <!-- 
+  <li class="nav-item">
+    <a class="nav-link" href="#download-application-documents">Download Application Documents</a>
+  </li> -->
+
+            <!-- <li class="nav-item">
+    <a class="nav-link" href="#updates-and-responses-to-applicant-questions">Updates and Responses to Applicant Questions</a>
+  </li>  -->
+
+            <li class="nav-item">
+              <a class="nav-link" href="#reporting">Reporting</a>
+            </li>
+
+
+          </ul>
+        </div>
+
+
+
+
+
+
+
+        <div class="col-md-9">
+          <div id="grant-posting-new">
+
+
+
+
+            <h1 class="spacerBottom50">{{title}}</h1>
+
+            <!-- START CONTENT: --------------------------------------------------
+
+    Step 1: Enter HTML here
+    Step 2: Update routing in routes.js
+
+--------------------------------------------------------------------- -->
+            <!--<h1 id="ffy14-16-edward-byrne-memorial-justice-assistance-grant-program-notice-of-funding-opportunity-jag-operations">FFY14-16 Edward Byrne Memorial Justice Assistance Grant Program Notice of Funding Opportunity: JAG OPERATIONS</h1>
+-->
+
+
+
+            <h2 id="introduction">Introduction</h2>
+            <p>The Illinois Criminal Justice Information Authority is requesting proposals for continuing support for nine current
+              prosecution, public defense and court programs currently supported with ICJIA-administered JAG funds. Grants
+              will be made with Federal Fiscal Year 2014-16 (FFY14-16) Edward Byrne Memorial Justice Assistance Grant (JAG)
+              funds. A total of $600,000 in funding is available through this solicitation for use over a period of 12 months.
+              Awards resulting from this NOFO will range from a minimum of $40,000 to a maximum of $225,000.</p>
+            <p>
+              <strong>Application deadline: 11:59 p.m., April 16, 2018.</strong>
+            </p>
+            <h2 id="program-requirements">Program Requirements</h2>
+            <p>Applicants may request funding for up to 12-months of program activity. Applications must:</p>
+            <ul>
+              <li>Demonstrate the effectiveness of the current program; and</li>
+              <li>Include the agency’s active participation in a data-driven planning process that objectively assesses jurisdictional
+                or agency needs.</li>
+            </ul>
+            <p>Support for program beyond this bridge funding is contingent on:</p>
+            <ul>
+              <li>Completion of a planning component.</li>
+              <li>Demonstrated ongoing need for the program.</li>
+              <li>Satisfactory performance.</li>
+              <li>Availability of JAG funds.</li>
+            </ul>
+            <p>Applicants are required to set reasonable, quantifiable benchmarks for program performance by completing the
+              Goals, Objectives and Performance Metrics table for the requested project. As this opportunity may continue
+              support for a range of program types, the objectives and performance measures must be crafted to address the
+              unique activities of each project. Applicants should be cognizant of supplanting restrictions detailed in the
+              NOFO. However, all projects funded must address the common goal defined in the NOFO.</p>
+            <p>Funded programs will be required to submit quarterly progress reports that will at a minimum include these objectives.
+              Applicants may list additional support service objectives for the program.</p>
+            <h2 id="eligibility">Eligibility Information</h2>
+            <p>Agencies must be pre-qualified through the Grant Accountability and Transparency Act (GATA) Grantee Portal,
+              <a href="http://www.grants.illinois.gov">www.grants.illinois.gov</a>, to become eligible to apply for an award. Applicants must have completed the GATA
+              pre-qualification process and received approval of their Internal Control Questionnaire (ICQ) by the date of
+              application to be considered for funding. Applications from agencies that have not received ICQ approval will
+              not be reviewed.</p>
+            <h2 id="eligible-applicants">Eligible Applicants</h2>
+            <p>Eligible applicants are limited to counties and State agencies requesting continuing funding for prosecution,
+              public defense, and courts programs currently supported by ICJIA-administered JAG funds. The eligible programs
+              are:
+            </p>
+            <ul>
+              <li>Office of the State Appellate Defender Rural Defense Program</li>
+              <li>Office of the State Appellate Prosecutor Systemic Sentencing Appeals Program</li>
+              <li>Office of the Cook County State’s Attorney Human Trafficking Program</li>
+              <li>Office of the Cook County State’s Attorney Community Justice Center Program</li>
+              <li>Office of the Cook County Public Defender DNA Program</li>
+              <li>Office of the Cook County Public Defender Mitigation Program</li>
+              <li>Winnebago County Youth Recovery Court</li>
+              <li>Center of Excellence</li>
+              <li>Coles/Cumberland Sex Offender Supervision program</li>
+            </ul>
+            <h2 id="notice-of-funding-opportunity">View the Notice of Funding Opportunity</h2>
+            <a href="/static/grants/20180227JAGoperations/JAGoperationsNOFO.pdf">Download the Notice of Funding Opportunity</a>
+            <h2 id="deadline">Deadline</h2>
+            <p>Completed application materials must be emailed to
+              <a href="mailto:CJA.JAGNOFO@Illinois.gov">CJA.JAGNOFO@Illinois.gov</a> by
+              <strong>11:59 p.m., April 16, 2018</strong>, to be considered for funding. Proposals will not be accepted by mail,
+              fax, or in-person. Incomplete applications will not be reviewed. Late submissions will not be reviewed.</p>
+            <h2 id="available-funds">Available Funds</h2>
+            <p>A total of $600,000 in funding is available through this solicitation. Funding minimum and maximum values are
+              based on the population served, according to the following standards:</p>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th style="text-align:center">Population of Area Served</th>
+                  <th style="text-align:center">Minimum Award</th>
+                  <th style="text-align:center">Maximum Award</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style="text-align:center">1 - 499,999</td>
+                  <td style="text-align:center">$40,000</td>
+                  <td style="text-align:center">$75,000</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">500,000 - 1,499,999</td>
+                  <td style="text-align:center">$40,000</td>
+                  <td style="text-align:center">$125,000</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">1,500,000 - 4,999,999</td>
+                  <td style="text-align:center">$40,000</td>
+                  <td style="text-align:center">$175,000</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">5,000,000 +</td>
+                  <td style="text-align:center">$40,000</td>
+                  <td style="text-align:center">$225,000</td>
+                </tr>
+              </tbody>
+            </table>
+            <h3 id="period-of-performance">Period of Performance</h3>
+            <p>Funding is available for the period of
+              <strong>July 1, 2018 through June 30, 2019</strong>. Based on program performance and fund availability, the Authority
+              may recommend allocation of funding to support an additional 24 months.</p>
+            <h3 id="contact-information">Contact Information</h3>
+            <p>Greg Stevens</p>
+            <p>Illinois Criminal Justice Information Authority</p>
+            <p>300 West Adams, Suite 200</p>
+            <p>Chicago, IL 60606</p>
+            <p>
+              <a href="mailto:Gregory.Stevens@Illinois.gov">Gregory.Stevens@Illinois.gov</a>
+            </p>
+            <h3 id="required-technical-assistance-recordings">Required Technical Assistance Recordings</h3>
+            <p>Technical assistance recordings providing additional information regarding this funding opportunity will be available
+              for viewing beginning at
+              <strong>1:30 p.m. on February 21, 2018</strong>.</p>
+            <p>
+              <a href="https://youtu.be/ZGouYw4XBwU">Recording 1: JAG Courts NOFO Programmatic Technical Assistance Recording (16:41)</a>
+            </p>
+            <p>
+              <a href="https://www.youtube.com/embed/zPaIPFXZvmg">Recording 2: General Requirements (11:19)</a>
+            </p>
+            <p>
+              <a href="https://www.youtube.com/embed/sQYCekU2pIw">Recording 3: GATA Budget (19:03)</a>
+            </p>
+            <p>
+              <a href="https://www.youtube.com/embed/mjp5PZx0oaY">Recording 4: Indirect Costs (6:18)</a>
+            </p>
+            <p>
+              <a href="https://www.youtube.com/embed/Q8UaLYqslJs">Recording 5: Prior Approvals (6:05)</a>
+            </p>
+            <p>Information provided during this webinar recording will be unofficial and not binding on the state.</p>
+            <h2 id="notices-of-intent">Notices of Intent</h2>
+            <p>Agencies interested in submitting an application are asked to submit an online Letter of Intent by
+              <strong>11:59 p.m., March 28, 2018</strong>.</p>
+            <p>
+              <a href="https://www.surveygizmo.com/s3/4201907/JAG-Operational-Effectiveness-NOFO-Notice-of-Intent">Complete Notice of Intent</a>
+            </p>
+            <h2 id="timeline">Timeline:</h2>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th style="text-align:center">Task</th>
+                  <th style="text-align:center">Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style="text-align:center">NOFO Posted</td>
+                  <td style="text-align:center">
+                    <strong>February 28, 2018</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">Technical assistance recordings available</td>
+                  <td style="text-align:center">February 28, 2018</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">Notices of Intent due</td>
+                  <td style="text-align:center">March 28, 2018</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">NOFO question submission deadline</td>
+                  <td style="text-align:center">April 9, 2018</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">
+                    <strong>Applications due</strong>
+                  </td>
+                  <td style="text-align:center">
+                    <strong>11:59 p.m., April 16, 2018</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">Budget Committee review/approval of recommended designations</td>
+                  <td style="text-align:center">June 2018</td>
+                </tr>
+                <tr>
+                  <td style="text-align:center">Projected program start date</td>
+                  <td style="text-align:center">July 1, 2018</td>
+                </tr>
+              </tbody>
+            </table>
+            <h2 id="reporting">Reporting</h2>
+            <p>Recipients must submit quarterly financial reports, quarterly progress reports, final financial and progress
+              reports, and, if applicable, an annual audit report in accordance with the CFR Part 200 Uniform Requirements.
+              Future awards and fund drawdowns may be withheld if reports are delinquent.</p>
+            <p>In addition, funded programs will be required to report performance measures through the U.S. Department of Justice
+              Bureau of Justice Assistance portal at
+              <a href="https://bjapmt.ojp.gov">https://bjapmt.ojp.gov</a>.</p>
+            <h2 id="required-documents">Required Documents</h2>
+            <p>The following documents must be emailed to
+              <a href="mailto:CJA.JAGNOFO@Illinois.gov">CJA.JAGNOFO@Illinois.gov</a> by the
+              <strong>11:59 p.m., April 16, 2018</strong>, deadline for application review. Click the links below to download copies.</p>
+            <ul>
+              <li>
+                <a href="/static/grants/20180227JAGoperations/JAGoperationsAPPLICATION.docx">Completed and signed Uniform State Grant Application (Word)</a>
+              </li>
+              <li>
+                <a href="/static/grants/20180227JAGoperations/JAGoperationsNARRATIVE.docx">Completed Program Narrative (Word)</a>
+              </li>
+              <li>
+                <a href="/static/grants/20180227JAGoperations/JAGoperationsBUDGET.xlsx">Completed Budget/Budget Narrative (Excel)</a>
+              </li>
+              <li>Agency Negotiated Indirect Cost Rate Agreement, if applicable (PDF)</li>
+            </ul>
+            <h2 id="questions">Questions</h2>
+            <p>Questions regarding this NOFO may be submitted to
+              <a href="mailto:CJA.JAGNOFO@Illinois.gov">CJA.JAGNOFO@Illinois.gov</a> until
+              <strong>11:59 p.m., April 9, 2018</strong>.</p>
+            <p>All substantive questions and responses will be posted. Updates to NOFO materials, and questions from applicants
+              and their responses are revised weekly.
+              <a href="/static/grants/20180227JAGoperations/JAGoperationsResponseToApplicantQuestion.pdf">View them here.</a>
+            </p>
+            <p>Due to the competitive nature of this solicitation, applicants may not discuss this opportunity directly or indirectly
+              with any Authority employee other than the respondent of this email address. Only written answers to questions
+              shall be binding on the state.
+            </p>
+
+
+
+            <!-- //END CONTENT --------------------------------------------------- -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    </div>
+
+    <sticky-sidebar />
+    <smooth-scroll />
+
+
+
+
+  </section>
+
+  </section>
+</template>
+
+<script>
+  import utilities from "@/utilities";
+  import PageHeader from "@/components/PageHeader";
+  import SmoothScroll from "@/components/SmoothScroll";
+  import StickySidebar from "@/components/StickySidebar";
+  //import StickySidebar from '@/components/StickySidebar'
+
+  const description = `The Illinois Criminal Justice Information Authority is requesting proposals for continuing support for nine current prosecution, public defense and court programs currently supported with ICJIA-administered JAG funds. Grants will be made with Federal Fiscal Year 2014-16 (FFY14-16) Edward Byrne Memorial Justice Assistance Grant (JAG) funds. A total of $600,000 in funding is available through this solicitation for use over a period of 12 months. Awards resulting from this NOFO will range from a minimum of $40,000 to a maximum of $225,000.`;
+  const title = utilities.titleCase(
+    "FFY14-16 Edward Byrne Memorial Justice Assistance Grant Program Notice of Funding Opportunity: JAG Operations"
+  );
+  const created = "2018-02-27";
+  const posted = "2018-02-28";
+  const expired = "2018-04-16";
+  const type = "grant";
+  const status = "live";
+
+  export default {
+    name: utilities.camelCase(utilities.nameCheck(title)),
+    pageData: {
+      created,
+      description,
+      posted,
+      expired,
+      title,
+      type,
+      status
+    },
+    mounted() { },
+    metaInfo: {
+      // title will be injected into parent titleTemplate
+      title,
+      meta: [
+        {
+          vmid: "description",
+          name: "description",
+          content: utilities.stripTags(description)
+        }
+      ]
+    },
+    components: {
+      PageHeader,
+      SmoothScroll,
+      StickySidebar
+    },
+    data() {
+      return {
+        title,
+        expired
+      };
+    }
+  };
+</script>
+
+<style lang="css" scoped>
+  h2 {
+    margin-top: 60px !important;
+  }
+
+  h3 {
+    margin-top: 40px !important
+  }
+
+  #grant-posting-new a {
+    padding-bottom: 10px;
+    text-decoration: underline;
+  }
+
+  a.nav-link {
+    text-decoration: none
+  }
+
+  #grant-posting-new a {
+    color: #2c2fb3 !important
+  }
+
+  table {
+    margin-bottom: 50px;
+  }
+
+  a.fix {
+    margin-top: 30px;
+    margin-bottom: 50px;
+    font-weight: 700;
+    text-decoration: none !important
+  }
+
+  a.btn-primary {
+    text-decoration: none !important
+  }
+</style>
