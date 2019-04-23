ff453c15c91017ac72bbca2783b897ccfaa78601
cschweda <cschweda@gmail.com>
2019-04-23 08:34:55 -0500

    fix: default grants to expired

--- src/views/Grants.vue
+++ src/views/Grants.vue
@@ -107,190 +107,175 @@
 </template>
 
 <script>
-    const description = `Grant Opportunities`
-    const title = "Grant Opportunities"
-    const created = '2017-03-26'
-    const posted = '2017-03-26'
-    const type = 'page'
-    const status = 'live'
-    import moment from 'moment'
-    import utilities from '@/utilities'
-    import PageHeader from '@/components/PageHeader'
-    import Grantfeed from 'components/Grantfeed'
+const description = `Grant Opportunities`;
+const title = "Grant Opportunities";
+const created = "2017-03-26";
+const posted = "2017-03-26";
+const type = "page";
+const status = "live";
+import moment from "moment";
+import utilities from "@/utilities";
+import PageHeader from "@/components/PageHeader";
+import Grantfeed from "components/Grantfeed";
 
-    export default {
-        name: 'Grants',
-        pageData: {
-            created,
-            description,
-            title,
-            type,
-            posted,
-            status
-        },
-        metaInfo: {
-            // title will be injected into parent titleTemplate
-            title,
-            meta: [{
-                vmid: 'description',
-                name: 'description',
-                content: utilities.stripTags(description)
-            }]
-        },
-        mounted() {
-            this.filter(this.filterType)
-                // $('#filterType').on('change', function(e) {
-                //   console.log(e)
-                // })
+export default {
+  name: "Grants",
+  pageData: {
+    created,
+    description,
+    title,
+    type,
+    posted,
+    status
+  },
+  metaInfo: {
+    // title will be injected into parent titleTemplate
+    title,
+    meta: [
+      {
+        vmid: "description",
+        name: "description",
+        content: utilities.stripTags(description)
+      }
+    ]
+  },
+  mounted() {
+    this.filter(this.filterType);
+    // $('#filterType').on('change', function(e) {
+    //   console.log(e)
+    // })
 
-            let vm = this
-            $(document).ready(function() {
-                $('.btn-filter').click(function() {
-                    var ft = $(this).val();
-                    console.log('Filter choice', ft)
-                    vm.filter(ft)
-                });
-            });
+    let vm = this;
+    $(document).ready(function() {
+      $(".btn-filter").click(function() {
+        var ft = $(this).val();
+        console.log("Filter choice", ft);
+        vm.filter(ft);
+      });
+    });
+  },
 
+  methods: {
+    checkExpire(d) {
+      if (d < this.now) {
+        return true;
+      }
+    },
+    changeFilter(event) {
+      //console.log(event.target.value)
+      this.filter(event.target.value);
+    },
+    isItExpired(d) {
+      // if (d < this.now) {
+      //   return true
+      // }
+      return d < this.now;
+    },
+    isItNew(d) {
+      let target = moment(d).add(this.weeksForNewBanner, "weeks");
+      return moment(target).isAfter(moment());
+    },
 
+    changeSortBy(event) {
+      //console.log(event.target.value)
+      this.sortFiltered(event.target.value);
+    },
+    changeSortDirection(event) {
+      //console.log(event.target.value)
+      this.sortDirection(event.target.value);
+    },
 
+    sortDirection(s) {
+      this.direction = s;
+      this.grants = _.orderBy(this.grants, this.sort, this.direction);
+    },
 
-        },
+    sortFiltered(s) {
+      this.sort = s;
+      this.grants = _.orderBy(this.grants, this.sort, this.direction);
+    },
 
-        methods: {
-            checkExpire(d) {
-                if (d < this.now) {
-                    return true
-                }
-            },
-            changeFilter(event) {
-                //console.log(event.target.value)
-                this.filter(event.target.value)
-            },
-            isItExpired(d) {
-                // if (d < this.now) {
-                //   return true
-                // }
-                return d < this.now
-            },
-            isItNew(d) {
-                let target = moment(d).add(this.weeksForNewBanner, 'weeks')
-                return moment(target).isAfter(moment())
-            },
+    filter(filterType) {
+      let self = this;
+      let filtered = [];
+      this.filterType = filterType;
+      this.warning = "";
+      // TODO: Try/Catch here
+      if (filterType === "expired") {
+        filtered = _.filter(this.$store.grants, function(o) {
+          return o.expired < self.now;
+        });
+      } else if (filterType === "current") {
+        filtered = _.filter(this.$store.grants, function(o) {
+          return o.expired > self.now;
+        });
+      } else {
+        filtered = this.$store.grants;
+      }
 
-            changeSortBy(event) {
-                //console.log(event.target.value)
-                this.sortFiltered(event.target.value)
-            },
-            changeSortDirection(event) {
-                //console.log(event.target.value)
-                this.sortDirection(event.target.value)
-            },
+      this.filtered = filtered;
+      this.grants = _.orderBy(this.filtered, this.sort, this.direction);
 
-            sortDirection(s) {
-                this.direction = s;
-                this.grants = _.orderBy(this.grants, this.sort, this.direction)
-
-            },
-
-
-
-            sortFiltered(s) {
-                this.sort = s;
-                this.grants = _.orderBy(this.grants, this.sort, this.direction)
-
-
-            },
-
-            filter(filterType) {
-                let self = this
-                let filtered = []
-                this.filterType = filterType
-                this.warning = '';
-                // TODO: Try/Catch here
-                if (filterType === 'expired') {
-                    filtered = _.filter(this.$store.grants, function(o) {
-                        return o.expired < self.now;
-                    })
-
-                } else if (filterType === 'current') {
-                    filtered = _.filter(this.$store.grants, function(o) {
-                        return o.expired > self.now;
-                    })
-                } else {
-                    filtered = this.$store.grants
-                }
-
-                this.filtered = filtered
-                this.grants = _.orderBy(this.filtered, this.sort, this.direction)
-
-                if (this.grants.length === 0) {
-                    this.warning = 'There are no ' + filterType + ' grants to display.'
-                }
-
-
-
-
-
-            }
-        },
-        components: {
-
-            PageHeader,
-            Grantfeed,
-
-        },
-        filters: {
-            limit: function(arr, limit) {
-                return arr.slice(0, limit)
-            },
-            moment: function(date) {
-                return moment(date).format('MMMM Do YYYY');
-            }
-        },
-        data() {
-            return {
-                title,
-                grants: _.orderBy(this.$store.grants, 'title', 'asc'),
-                now: moment().subtract(1, 'days'),
-                warning: '',
-                sort: 'posted',
-                direction: 'desc',
-                filtered: [],
-                isActive: true,
-                filterType: 'current',
-                selectedButton: '',
-                selected: 'A',
-                weeksForNewBanner: 1
-            }
-        },
+      if (this.grants.length === 0) {
+        this.warning = "There are no " + filterType + " grants to display.";
+      }
     }
+  },
+  components: {
+    PageHeader,
+    Grantfeed
+  },
+  filters: {
+    limit: function(arr, limit) {
+      return arr.slice(0, limit);
+    },
+    moment: function(date) {
+      return moment(date).format("MMMM Do YYYY");
+    }
+  },
+  data() {
+    return {
+      title,
+      grants: _.orderBy(this.$store.grants, "title", "asc"),
+      now: moment().subtract(1, "days"),
+      warning: "",
+      sort: "posted",
+      direction: "desc",
+      filtered: [],
+      isActive: true,
+      filterType: "expired",
+      selectedButton: "",
+      selected: "A",
+      weeksForNewBanner: 1
+    };
+  }
+};
 </script>
 
 <style lang="css">
-    .btn-filter {
-        font-size: 10px;
-        font-weight: 900
-    }
-    
-    .btn.active {
-        background: #152c61;
-        color: #fff
-    }
-    
-    .grant-news {
-        margin-bottom: 65px;
-    }
-    
-    .control-panel {
-        background: #fff;
-        font-size: 14px;
-        padding: 2px 0px 2px 0px;
-        text-transform: uppercase;
-        font-weight: 700
-    }
-    
-    .control-panel-spacer {
-        margin-left: 30px;
-    }
+.btn-filter {
+  font-size: 10px;
+  font-weight: 900;
+}
+
+.btn.active {
+  background: #152c61;
+  color: #fff;
+}
+
+.grant-news {
+  margin-bottom: 65px;
+}
+
+.control-panel {
+  background: #fff;
+  font-size: 14px;
+  padding: 2px 0px 2px 0px;
+  text-transform: uppercase;
+  font-weight: 700;
+}
+
+.control-panel-spacer {
+  margin-left: 30px;
+}
 </style>
